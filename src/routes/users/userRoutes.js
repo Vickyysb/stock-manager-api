45e3d2a3f72df08be680995ecb1b5dcbf25f376e
Vitoria <vitoriasb95@gmail.com>
2021-06-15 20:46:35 -0300

    Endpoints delete/soft delete/update/find one de usuários

--- src/routes/users/userRoutes.js
+++ src/routes/users/userRoutes.js
@@ -11,6 +11,9 @@ const path = '/users';
 
 userRoute.post(`${path}/create`, userController.createUser.bind(userController));
 userRoute.get(`${path}`, userController.getUsers.bind(userController));
-
+userRoute.get(`${path}/:id`, userController.getUser.bind(userController));
+userRoute.get(`${path}/delete/:id`, userController.deleteUser.bind(userController));
+userRoute.get(`${path}/delete/soft/:id`, userController.deleteUserSoft.bind(userController));
+userRoute.post(`${path}/update/:id`, userController.updateUser.bind(userController));
 
 module.exports =  userRoute;
